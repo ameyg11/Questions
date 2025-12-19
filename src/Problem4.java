4da7899444aecf30432c068f60fcaf11e1068ca4
Amey <ameygawade11@gmail.com>
2025-12-19 11:44:13 +0530

    solved Problem 4

---
+++ src/Problem4.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+public class Problem4 {
+    public static void main(String[] args) {
+        int[] numbers = {1,2,8,9,12,46,76,82,15,20,30};
+
+        HashMap<Integer, Integer> result = new HashMap<>();
+
+        for (int i = 1; i <= 9; i++) {
+            int count = 0;
+            for (int num : numbers) {
+                if (num % i == 0) {
+                    count++;
+                }
+            }
+            result.put(i, count);
+        }
+
+        System.out.println(result);
+    }
+}
