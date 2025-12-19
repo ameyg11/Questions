2edb77f6a109415ecbc635983dcd6518f7c5f53b
Amey <ameygawade11@gmail.com>
2025-12-19 11:31:40 +0530

    initialized repo. and solved Problem1

---
+++ src/Problem1.java
@@ -0,0 +1,38 @@
+class Calculator {
+    double a;
+    double b;
+    String operation;
+
+    Calculator(double a, double b, String operation) {
+        this.a = a;
+        this.b = b;
+        this.operation = operation;
+    }
+
+    double calculate() {
+        switch (operation) {
+            case "+":
+                return a + b;
+            case "-":
+                return a - b;
+            case "*":
+                return a * b;
+            case "/":
+                if (b == 0) {
+                    System.out.println("Division by zero not allowed");
+                    return 0;
+                }
+                return a / b;
+            default:
+                System.out.println("Invalid operation");
+                return 0;
+        }
+    }
+}
+
+public class Problem1 {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator(10, 5, "+");
+        System.out.println("Result: " + calc.calculate());
+    }
+}
